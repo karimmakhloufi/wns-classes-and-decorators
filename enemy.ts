f236d49d40e332e16a42483f92919383bf2441d4
Karim Makhloufi <karim@kazoo.agency>
2020-11-09 22:10:46 +0100

    Removed the constructor, as we now use class transformer to create out instance of class

--- enemy.ts
+++ enemy.ts
@@ -11,13 +11,6 @@ export class Enemy {
   emoji;
   attackName;
   enemy;
-  constructor(name, level, emoji, attackName) {
-    this.name = name;
-    this.level = level;
-    this.emoji = emoji;
-    this.attackName = attackName;
-    this.enemy = true;
-  }
   attacks() {
     return `${this.name} ${this.attackName} you!`;
   }
