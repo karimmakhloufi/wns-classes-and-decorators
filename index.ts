2342baab5810d3f0e1b24d19cd7951a71e862d65
Karim Makhloufi <karim@kazoo.agency>
2020-11-09 21:53:28 +0100

    Use class validator to validate spider Enemy

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
+import { validate } from "class-validator";
 import { Enemy } from "./enemy";
 
 console.log("Hello from node");
 
-const spider = new Enemy("spider", 1, "🕷", "bites");
+const spider = new Enemy("spider", 12, "🕷", "bites");
 const bear = new Enemy("bear", 20, "🐻", "scratches");
 const sname = new Enemy("snake", 6, "🐍", "bites");
+
+validate(spider).then((errors) => console.log(errors));
 
 console.log(spider);
 
