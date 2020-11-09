7023e12349c5ad0c6ecdf5b39cb2e0e3736881cf
Karim Makhloufi <karim@kazoo.agency>
2020-11-09 22:20:41 +0100

    Typed Enemy properties, now that the type cannot be infered

--- enemy.ts
+++ enemy.ts
@@ -1,16 +1,15 @@
 import { IsInt, Min, Max } from "class-validator";
 
 export class Enemy {
-  name;
+  name: string;
 
   @IsInt()
   @Min(0)
   @Max(10)
   level: number;
 
-  emoji;
-  attackName;
-  enemy;
+  emoji: string;
+  attackName: string;
   attacks() {
     return `${this.name} ${this.attackName} you!`;
   }
