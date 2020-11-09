0af43b3e61bb1e01358f5a2f5e1843c74155211a
Karim Makhloufi <karim@kazoo.agency>
2020-11-09 20:14:02 +0100

    Logging the spider to show that it is of type enemy

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const spider = new Enemy("spider", 1, "🕷", "bites");
 const bear = new Enemy("bear", 20, "🐻", "scratches");
 const sname = new Enemy("snake", 6, "🐍", "bites");
 
+console.log(spider);
+
 console.log(spider.attacks());
